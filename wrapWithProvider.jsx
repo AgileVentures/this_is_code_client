61fc292fc23b29dd808817069832bd7aa4f83ae3
Thomas <thomas@craftacademy.se>
2019-12-12 07:28:14 +0100

    cleans up code in wrapWithProvider internal override

--- wrapWithProvider.jsx
+++ wrapWithProvider.jsx
@@ -5,12 +5,12 @@ import createStore from "./src/state/store/createStore"
 
 // eslint-disable-next-line react/display-name,react/prop-types
 const wrapWithProvider = ({ element }) => {
-  // Instantiating store in `wrapRootElement` handler ensures:
-  //  - there is fresh store for each SSR page
-  //  - it will be called only once in browser, when React mounts
   const store = createStore()
-  window.store = store
-  return <Provider store={store}>{element}</Provider>
+
+  return (
+    <Provider store={store}>
+      {element}
+    </Provider>)
 }
 
 export default wrapWithProvider
