c0301f545b4c1fdc6b429569ab963e9fdd04bf9a
Aditya Naik <naik.aditya@gmail.com>
2020-02-05 11:25:00 +0100

    Adds events and prices to course details

--- src/components/CourseDetails.jsx
+++ src/components/CourseDetails.jsx
@@ -1,59 +1,87 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { HIDE_COURSE_DETAILS } from '../state/actions/actionTypes'
+import React from "react";
+import moment from "moment";
+import { connect } from "react-redux";
+import { HIDE_COURSE_DETAILS } from "../state/actions/actionTypes";
 
-import {
-  Modal,
-} from 'carbon-components-react'
+import { Modal, Accordion, AccordionItem } from "carbon-components-react";
 
-const CourseDetails = (props) => {
-  let { course } = props
+const CourseDetails = props => {
+  let { course } = props;
   return (
-    <div style={{ position: 'fixed', top: '0px', zIndex: '999' }}>
+    <div style={{ position: "fixed", top: "0px", zIndex: "999" }}>
       <Modal
         hasScrollingContent
         focusTrap={true}
         iconDescription="Close"
         modalHeading={props.course.title}
         open
-        passiveModal={true}
+        transactionalModal={true}
         modalAriaLabel={props.course.title}
-        // primaryButtonText={props.course.title}
-        secondaryButtonText="Cancel"
+        primaryButtonText={
+          props.course.displayPrice > 0
+            ? `Buy for $${props.course.displayPrice}`
+            : "This course is FREE to attend!"
+        }
+        secondaryButtonText={
+          props.course.soloPrice
+            ? `Buy Solo for $${props.course.soloPrice} `
+            : "Solo purchase not Available for this course"
+        }
         primaryButtonDisabled={false}
+        secondaryButtonDisabled={!props.course.soloPrice}
         onRequestClose={() => props.hideCourseDetails()}
-        size='lg'
+        size="lg"
         selectorPrimaryFocus="img"
       >
         <img
-          alt='Card cover'
-          style={{ width: 'auto', minHeight: '50%', objectFit: 'cover' }}
+          alt="Card cover"
+          style={{ width: "auto", minHeight: "50%", objectFit: "cover" }}
           src={props.course.coverImage}
         />
 
         <p className="bx--modal-content__text">
-          <strong>Note:</strong> Enrollment opens up in January 2020. Stay tuned.
+          <strong>Note:</strong> Enrollment opens up in January 2020. Stay
+          tuned.
         </p>
 
-        <p className="bx--modal-content__text">
-          {props.course.description}
-        </p>
+        <p className="bx--modal-content__text">{props.course.description}</p>
         <p className="bx--modal-content__text">
           {`Host: ${course.owner.firstName} ${course.owner.lastName}`}
         </p>
 
         <p className="bx--modal-content__text">
-          {`${course.events.length} instructor led session${course.events.length !== 1 ? 's' : ''}`}
+          {`${course.events.length} instructor led session${
+            course.events.length !== 1 ? "s" : ""
+          }`}
         </p>
+        <Accordion>
+          {props.course.events.length > 0 ? (
+            props.course.events.map(event => {
+              return (
+                <div>
+                  <AccordionItem title={event.title}>
+                    <p>
+                      Date: {moment(event.startDate).format("Do MMM HH:mm")} to{" "}
+                      {moment(event.endDate).format("Do MMM HH:mm")}
+                    </p>
+                    About: {event.description}
+                  </AccordionItem>
+                </div>
+              );
+            })
+          ) : (
+            <></>
+          )}
+        </Accordion>
       </Modal>
     </div>
-  )
-}
+  );
+};
 
 const mapDispatchToProps = dispatch => {
   return {
     hideCourseDetails: () => dispatch({ type: HIDE_COURSE_DETAILS })
-  }
-}
+  };
+};
 
-export default connect(null, mapDispatchToProps)(CourseDetails)
+export default connect(null, mapDispatchToProps)(CourseDetails);
