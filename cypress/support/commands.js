49dc3624f056c28a8f23a3946b7612967677268b
Aditya Naik <naik.aditya@gmail.com>
2020-04-16 11:40:08 +0200

    Refactors tests to extract common assertion

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('checkText',(element, text) => {
+  cy.get(element).should('contain.text', text)
+})
