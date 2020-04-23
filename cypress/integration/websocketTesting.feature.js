834cecbb1860ebaa1706f46541c196d7c56c9656
Aditya Naik <naik.aditya@gmail.com>
2020-04-23 14:26:53 +0200

    Uses manual-web-socket package to mock out websocket server and test the websocket implementation

--- cypress/integration/websocketTesting.feature.js
+++ cypress/integration/websocketTesting.feature.js
@@ -1,5 +1,7 @@
 /// <reference types="Cypress" />
 
+const manualWebSocket = require('manual-web-socket')
+
 describe('Event link is updated via websocket', () => {
   beforeEach('', () => {
     cy.server()
@@ -9,77 +11,120 @@ describe('Event link is updated via websocket', () => {
       status: 200,
       response: 'fixture:course-list.json',
     })
-  })
-  it('Event link is updated via websocket', () => {
     cy.route({
       method: 'GET',
       url: 'http://localhost:3000/auth/validate_token',
       status: 200,
       response: 'fixture:validate-token.json',
     })
-    cy.visit('/getting-started/student')
-    cy.get('.bx--article-card').last().click()
-    cy.get('.bx--modal-header__heading').should(
-      'contain.text',
-      'Connecting ACL2'
-    )
-    cy.get('.bx--modal-content__text').should(
-      'contain.text',
-      'Use the mobile HTTP firewall, then you can quantify the 1080p panel!Host: undefined undefined3 instructor led sessions'
-    )
-    cy.get('.bx--accordion__title').should('have.length', 3)
-    cy.get('.bx--accordion__title').each((element, index) => {
-      cy.get(element[0])
-        .should('have.text', `Connecting ACL2 - part ${index + 1}`)
-        .click()
+    cy.route({
+      method: 'POST',
+      url: 'https://localhost:5000/auth/login',
+      status: 200,
+      response: 'fixture:course-list.json',
     })
-    cy.get('.bx--accordion__content').each((element, index) => {
-      cy.get(element[0]).should(
-        'not.contain.html',
-        '<a href="https://hangouts.google.com/call" target="_blank" rel="noopener noreferrer" style="text-decoration: none; color: black;">Join hangout Here</a>'
+  })
+
+  it('Event link is updated via websocket', () => {
+    const checkArticleDetails = () => {
+      cy.get('.bx--article-card').last().click()
+      cy.get('.bx--modal-header__heading').should(
+        'contain.text',
+        'Connecting ACL2'
       )
+      cy.get('.bx--modal-content__text').should(
+        'contain.text',
+        'Use the mobile HTTP firewall, then you can quantify the 1080p panel!Host: undefined undefined3 instructor led sessions'
+      )
+      cy.get('.bx--accordion__title').should('have.length', 3)
+      cy.get('.bx--accordion__title').each((element, index) => {
+        cy.get(element[0])
+          .should('have.text', `Connecting ACL2 - part ${index + 1}`)
+          .click()
+      })
+    }
+    cy.visit('/getting-started/student').then(() => {
+      checkArticleDetails()
+      cy.get('.bx--accordion__content').each((element, index) => {
+        cy.get(element[0]).should(
+          'not.contain.html',
+          '<a href="https://hangouts.google.com/call" target="_blank" rel="noopener noreferrer" style="text-decoration: none; color: black;">Join hangout Here</a>'
+        )
+      })
     })
-    const event = [
-      {
-        id: 88,
-        title: 'Connecting ACL2 - part 1',
-        description:
-          'Use the mobile HTTP firewall, then you can quantify the 1080p panel!',
-        startDate: 'Tue, 11 Feb 2020 14:59:59 +0000',
-        endDate: 'Tue, 11 Feb 2020 15:29:59 +0000',
-        conference_link: 'https://hangouts.google.com/call',
+    cy.visit('/getting-started/student', {
+      onBeforeLoad(win) {
+        var script = win.document.createElement('script')
+        script.innerText = manualWebSocket.getScript()
+        win.document.head.appendChild(script)
+        win.mws.track(['wss://localhost:5000/connection'])
       },
-      {
-        id: 89,
-        title: 'Connecting ACL2 - part 2',
-        description:
-          'Use the mobile HTTP firewall, then you can quantify the 1080p panel!',
-        startDate: 'Wed, 12 Feb 2020 14:59:59 +0000',
-        endDate: 'Wed, 12 Feb 2020 15:29:59 +0000',
-        conference_link: 'https://hangouts.google.com/call',
-      },
-      {
-        id: 90,
-        title: 'Connecting ACL2 - part 3',
-        description:
-          'Use the mobile HTTP firewall, then you can quantify the 1080p panel!',
-        startDate: 'Thu, 13 Feb 2020 14:59:59 +0000',
-        endDate: 'Thu, 13 Feb 2020 15:29:59 +0000',
-        conference_link: 'https://hangouts.google.com/call',
-      },
-    ]
-
-    cy.window()
-      .its('store')
-      .invoke('dispatch', { type: 'UPDATE_EVENTS', payload: event })
-
-    cy.wait(5000)
-
-    cy.get('.bx--accordion__content').each((element, index) => {
-      cy.get(element[0]).should(
-        'contain.html',
-        '<a href="https://hangouts.google.com/call" target="_blank" rel="noopener noreferrer" style="text-decoration: none; color: black;">Join hangout Here</a>'
+    }).then((win) => {
+      const mws = win.mws
+      const connections = mws.trackedConnections
+      let trackedConnection
+      trackedConnection = connections.getByUrl(
+        'wss://localhost:5000/connection'
       )
+      trackedConnection.readyState = mws.readyState.OPEN
+      const message1 = 'connection open with client'
+
+      const event = {
+        message: {
+          events: [
+            {
+              id: 88,
+              title: 'Connecting ACL2 - part 1',
+              description:
+                'Use the mobile HTTP firewall, then you can quantify the 1080p panel!',
+              startDate: 'Tue, 11 Feb 2020 14:59:59 +0000',
+              endDate: 'Tue, 11 Feb 2020 15:29:59 +0000',
+              conference_link: 'https://hangouts.google.com/call',
+              coverImage:
+                'https://this-is-code.s3.eu-north-1.amazonaws.com/zpdcgiWFFoivQtM7qtYjkVFe?response-content-disposition=inline%3B%20filename%3D%22cover_88.jpg%22%3B%20filename%2A%3DUTF-8%27%27cover_88.jpg\u0026response-content-type=image%2Fjpeg\u0026X-Amz-Algorithm=AWS4-HMAC-SHA256\u0026X-Amz-Credential=AKIAIKNDI6ICZXWA675Q%2F20200213%2Feu-north-1%2Fs3%2Faws4_request\u0026X-Amz-Date=20200213T085027Z\u0026X-Amz-Expires=3600\u0026X-Amz-SignedHeaders=host\u0026X-Amz-Signature=9bf48ae7000af73589dba6f27a8a8d47ec5785f6cbc4f40869856f4586e1c1ed',
+            },
+            {
+              id: 89,
+              title: 'Connecting ACL2 - part 2',
+              description:
+                'Use the mobile HTTP firewall, then you can quantify the 1080p panel!',
+              startDate: 'Wed, 12 Feb 2020 14:59:59 +0000',
+              endDate: 'Wed, 12 Feb 2020 15:29:59 +0000',
+              conference_link: 'https://hangouts.google.com/call',
+              coverImage:
+                'https://this-is-code.s3.eu-north-1.amazonaws.com/4NUS25YRL3XueZvRF36iRDZj?response-content-disposition=inline%3B%20filename%3D%22cover_89.jpg%22%3B%20filename%2A%3DUTF-8%27%27cover_89.jpg\u0026response-content-type=image%2Fjpeg\u0026X-Amz-Algorithm=AWS4-HMAC-SHA256\u0026X-Amz-Credential=AKIAIKNDI6ICZXWA675Q%2F20200213%2Feu-north-1%2Fs3%2Faws4_request\u0026X-Amz-Date=20200213T085027Z\u0026X-Amz-Expires=3600\u0026X-Amz-SignedHeaders=host\u0026X-Amz-Signature=d08a7f36699cc4fc4565a0d9c8628294588a79b1bca48bbdab15f21b343c1c0a',
+            },
+            {
+              id: 90,
+              title: 'Connecting ACL2 - part 3',
+              description:
+                'Use the mobile HTTP firewall, then you can quantify the 1080p panel!',
+              startDate: 'Thu, 13 Feb 2020 14:59:59 +0000',
+              endDate: 'Thu, 13 Feb 2020 15:29:59 +0000',
+              conference_link: 'https://hangouts.google.com/call',
+              coverImage:
+                'https://this-is-code.s3.eu-north-1.amazonaws.com/q5Nq5F3NpxPEojcSyKG1jKT1?response-content-disposition=inline%3B%20filename%3D%22cover_90.jpg%22%3B%20filename%2A%3DUTF-8%27%27cover_90.jpg\u0026response-content-type=image%2Fjpeg\u0026X-Amz-Algorithm=AWS4-HMAC-SHA256\u0026X-Amz-Credential=AKIAIKNDI6ICZXWA675Q%2F20200213%2Feu-north-1%2Fs3%2Faws4_request\u0026X-Amz-Date=20200213T085027Z\u0026X-Amz-Expires=3600\u0026X-Amz-SignedHeaders=host\u0026X-Amz-Signature=842af8578fe8f3984e33b88877272cde0e048673b5227507e494b6eaf64e70d0',
+            },
+          ],
+        },
+      }
+      
+      const payload = { data: JSON.stringify(event)}
+      trackedConnection.addServerScenario(
+        'connection open with client',
+        (connection, message) => {
+          connection.reciveMessage(payload)
+          console.log(message, event)
+        }
+      )
+      trackedConnection.send(message1)
+      checkArticleDetails()
+      cy.get('.bx--accordion__content').each((element, index) => {
+        cy.get(element[0]).should(
+          'contain.html',
+          '<a href="https://hangouts.google.com/call" target="_blank" rel="noopener noreferrer" style="text-decoration: none; color: black;">Join hangout Here</a>'
+        )
+      })
     })
   })
 })
--- src/components/WebsocketHandler.jsx
+++ src/components/WebsocketHandler.jsx
@@ -44,11 +44,11 @@ const WebsocketHandler = () => {
       console.log('connection closed')
       // history.push('/')
     }
-    wss.onerror = error => {
+    wss.onerror = (error) => {
       console.log('connection error', error)
       wss.close()
     }
-    wss.onmessage = message => {
+    wss.onmessage = (message) => {
       let updatedEvents = JSON.parse(message.data).message.events
       if (JSON.stringify(updatedEvents) !== JSON.stringify(events)) {
         events = JSON.parse(message.data).message.events
@@ -62,35 +62,7 @@ const WebsocketHandler = () => {
   useEffect(() => {
     !nodeAuth && nodeAuthentication()
   }, [])
-  // useEffect(() => {}, [nodeAuth])
   return null
 }
 
 export default WebsocketHandler
-
-// Also need to handle auth with node JS api - get the headers and authenticate with node
-
-// handle the cors settings in gatsby
-
-// Actions needed
-
-/* 
-
-- Handle auth with nodejs
-  - set up headers properly
-  - set special auth status in redux
-
-- launch websockets client only if user has signed in - done
-
-- hold events in state
-- match incoming message with state, dispatch update_event action only if there is change
-
-- Update redux reducer with update event action - loop through incoming events, update connection link 
-
-- Update display events/course component - render link and event information
-
-- Maybe show a popup saying link is available for a specific event
-
-- close websocket on logout
-
-*/
